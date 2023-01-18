21712599e106ff07049c08a6d9b920d6cae33a1a
KamilMusialowski <87933506+KamilMusialowski@users.noreply.github.com>
2023-01-18 16:07:56 +0100

    Initial Project Add

---
+++ src/test/java/musialowski/kamil/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package musialowski.kamil.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
