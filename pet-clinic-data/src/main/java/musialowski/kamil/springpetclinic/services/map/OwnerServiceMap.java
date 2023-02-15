d592a46ba886556da911915b0dfa142d1f01266f
KamilMusialowski <87933506+KamilMusialowski@users.noreply.github.com>
2023-02-15 10:08:00 +0100

    Implements map based services. Closes #5.

---
+++ pet-clinic-data/src/main/java/musialowski/kamil/springpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,39 @@
+package musialowski.kamil.springpetclinic.services.map;
+
+import musialowski.kamil.springpetclinic.model.Owner;
+import musialowski.kamil.springpetclinic.services.OwnerService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return new Owner(); //to implement
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
