7e2838cdb66893c0d2367e6777ac9a9e27c9b606
KamilMusialowski <87933506+KamilMusialowski@users.noreply.github.com>
2023-02-22 13:17:48 +0100

    Added PetType Map Service. Closes #27.

---
+++ pet-clinic-data/src/main/java/musialowski/kamil/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package musialowski.kamil.springpetclinic.services;
+
+import musialowski.kamil.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+
+}
