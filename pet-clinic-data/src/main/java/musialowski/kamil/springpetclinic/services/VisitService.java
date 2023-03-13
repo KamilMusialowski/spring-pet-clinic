2184ae7a2baa83d99adb4040006d68d260e96f35
KamilMusialowski <87933506+KamilMusialowski@users.noreply.github.com>
2023-03-13 18:42:20 +0100

    Created Visit Service for Map Implementation. Closes #44.

---
+++ pet-clinic-data/src/main/java/musialowski/kamil/springpetclinic/services/VisitService.java
@@ -0,0 +1,8 @@
+package musialowski.kamil.springpetclinic.services;
+
+import musialowski.kamil.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+
+
+}
